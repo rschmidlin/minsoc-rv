3898d8e9f9f9b8e1d622d025dcfd32c1ee36e70c
Raul Schmidlin <raulschmidlin@gmail.com>
2026-06-08 01:26:17 +0200

    Started new strategy

--- rtl/verilog/ibex_wb_host_adapter.v
+++ rtl/verilog/ibex_wb_host_adapter.v
@@ -14,7 +14,9 @@
 //       At the moment, going directly to CTI 000 and negating CYC, normally CTI 111 and CYC 1 required
 //       for one cycle. Potentially negate STB while going for CTI 111 and CYC? 
 
-module ibex_wb_host_adapter (
+module ibex_wb_host_adapter #(
+  parameter MAX_FIFO_DEPTH = 2)
+  (
     input wire clk,
     input wire rst,
 
@@ -26,7 +28,7 @@ module ibex_wb_host_adapter (
     input wire [31:0] req_wdata,
     input wire [ 3:0] req_be,
 
-    output wire gnt,              // Combinatorial grant: high while collecting sequential requests
+    output reg gnt,              // Combinatorial grant: high while collecting sequential requests
 
     // Response
     output reg        resp_valid,
@@ -45,25 +47,42 @@ module ibex_wb_host_adapter (
     output reg [ 1:0] wb_bte
 );
 
-wire req_accepted;
+wire last_beat;
+reg fifo_wr_en;
+reg fifo_rd_en;
+wire fifo_full, fifo_empty;
+reg fifo_last_beat;
+reg fifo_req_we;
+reg [3:0] fifo_req_be;
+reg [31:0] fifo_req_wdata;
+reg [31:0] fifo_req_addr;
+
+// Store req_addr & req_be 
+fifo_fwft #(
+    .DEPTH_WIDTH(MAX_FIFO_DEPTH),
+    .DATA_WIDTH(70)
+  ) fifo_i(
+    .clk(clk),
+    .rst(rst),
+    .din({last_beat, req_we, req_be, req_wdata, req_addr}),
+    .wr_en(fifo_wr_en),
+    .full(fifo_full),
+    .dout({fifo_last_beat, fifo_req_we, fifo_req_be, fifo_req_wdata, fifo_req_addr}),
+    .rd_en(fifo_rd_en),
+    .empty(fifo_empty)
+);
+
+reg [31:0] req_addr_q;
+reg req_we_q;
+reg [3:0] req_be_q;
+reg [31:0] reg_wdata_q;
+
 wire [31:0] next_address;
 wire valid_req_address;
 
-reg [3:0] start_len;
-reg [3:0] transferred_len;
-reg [3:0] accepted_len;
-reg [31:0] req_addr_q;
-reg gnt_q;
-
-reg wb_new_cycle_req;
-reg [3:0] wb_new_cycle_len;
-
-assign req_accepted = (accepted_len != start_len);
 assign next_address = req_addr_q + 'd4;
 assign valid_req_address = (req_addr == next_address);
-
-wire [3:0] transfer_remaining_len = accepted_len - transferred_len;
-wire [3:0] accepted_len_in_window = accepted_len - start_len;
+assign last_beat = (req_we ^ req_we_q) | (req_be ^ req_be_q) | !valid_req_address;
 
 localparam IDLE = 2'b00;
 localparam ACCEPT = 2'b01;
@@ -76,128 +95,106 @@ reg [1:0] wb_state;
 always @(posedge clk) begin
   if (rst) begin
     ib_state <= IDLE;
-    req_addr_q <= 32'h0000_0000;
-    gnt_q <= 1'b0;
-    accepted_len <= 'd0;
-    start_len <= 'd0;
-    wb_new_cycle_req <= 1'b0;
-    wb_new_cycle_len <= 'd0;
+    gnt <= 1'b0;
+    fifo_wr_en <= 1'b0;
   end
   else begin
     case (ib_state)
       IDLE: begin
-        gnt_q <= 1'b0;
-        if ((wb_state == IDLE) && (accepted_len == transferred_len)) begin  // accepted_len impacts ongoing burst if cleared too early
-          req_addr_q   <= 32'h0000_0000;
-          start_len <= accepted_len;
-        end
-        if (req_valid && !wb_cyc) begin
+        fifo_wr_en <= 1'b0;
+        gnt <= 1'b0;
+        if (req_valid) begin
           ib_state <= ACCEPT;
-          wb_new_cycle_req <= 1'b0;
         end
       end
       ACCEPT: begin
-        gnt_q <= 1'b0;
-        if (transfer_remaining_len > 'd2) begin
+        fifo_wr_en <= 1'b0;
+        gnt <= 1'b0;
+        if (fifo_full) begin
           ib_state <= STALL;        // needed for write data otherwise we lose the data in the WB_FSM
         end
-        else if (req_valid 
-        && (!req_accepted 
-        || valid_req_address)) begin
-          accepted_len <= accepted_len + 'd1;
+        else if (req_valid && !fifo_wr_en && 
+          (fifo_empty || (req_addr == (req_addr_q + 'h4)))) begin
+          fifo_wr_en <= 1'b1;
+          gnt <= 1'b1;
           req_addr_q <= req_addr;
-          gnt_q <= 1'b1;
+          req_we_q <= req_we;
+          req_be_q <= req_be;
+          //reg_wdata_q <= reg_wdata;
         end
-        else if (!gnt_q) begin  // after gnt is cleared new address is there, STALL if no request or address
+        else if (!gnt) begin  // after gnt is cleared new address is there, STALL if no request or address
             ib_state <= STALL;
-            wb_new_cycle_req <= !valid_req_address;
-            wb_new_cycle_len <= accepted_len;
         end
       end
       STALL: begin
-        if (!req_valid
-            || !valid_req_address
-            || (accepted_len_in_window == req_len)) begin
+        fifo_wr_en <= 1'b0;
+        if (!req_valid) begin
           ib_state <= IDLE;
         end
-        else if (transfer_remaining_len <= 'd2) begin
-            if ((wb_state == IDLE) && (accepted_len == transferred_len)) begin  // if wb_fsm just finished a transaction, accepted_len must start from 0
-              req_addr_q   <= 32'h0000_0000;
-            end
-            ib_state <= ACCEPT;
+        else if (!fifo_full) begin
+          ib_state <= ACCEPT;
         end
       end
     endcase
   end
 end
 
-assign gnt = gnt_q;
-
-
 localparam ACTIVE = 2'b01;
 localparam FINISH = 2'b10;
 
-wire wb_pending;
-
-assign wb_pending = req_accepted && (transferred_len != accepted_len);
-
-wire [3:0] new_cycle_req_len = wb_new_cycle_len - transferred_len;
-wire [3:0] accepted_len_since_last_idle = accepted_len - start_len;
-
 always @(posedge clk) begin
   if (rst) begin
-    transferred_len <= 'd0;
     wb_cyc <= 1'b0;
     wb_stb <= 1'b0;
     wb_we <= 1'b0;
     wb_adr <= 32'h0000_0000;
     wb_dat_w <= 32'h0000_0000;
-    wb_sel <= req_be;
+    wb_sel <= 4'h0;
     wb_cti <= 3'b000;
     wb_bte <= 2'b00;
     wb_state <= IDLE;
+    fifo_rd_en <= 1'b0;
   end
   else begin
     resp_valid <= 1'b0;  // default every cycle
     case (wb_state)
       IDLE: begin
+        fifo_rd_en <= 1'b0;
         resp_valid <= 1'b0;
         wb_cyc <= 1'b0;
         wb_stb <= 1'b0;
         wb_we <= 1'b0;
         wb_adr <= 32'h0000_0000;
         wb_dat_w <= 32'h0000_0000;
-        wb_sel <= req_be;
+        wb_sel <= 4'h0;
         wb_cti <= 3'b000;
         wb_bte <= 2'b00;
 
-        if (wb_pending) begin
-          wb_state <= ACTIVE;
-          //transferred_len <= start_len;
+        if (!fifo_empty) begin
+          fifo_rd_en <= 1'b1;
           wb_cyc <= 1'b1;
           wb_stb <= 1'b1;
-          wb_we <= req_we;
-          wb_adr <= req_addr_q;
-          wb_dat_w <= req_wdata;
-          if (req_len > 'd1) begin
+          wb_we <= fifo_req_we;
+          wb_adr <= fifo_req_addr;
+          wb_dat_w <= fifo_req_wdata;
+          wb_sel <= fifo_req_be;
+          if (!fifo_last_beat && !fifo_req_we && !fifo_empty) begin
             wb_cti <= 3'b010;
             wb_bte <= 2'b00;
           end
+          wb_state <= ACTIVE;
         end
       end
       ACTIVE: begin
+        fifo_rd_en <= 1'b0;
         if (wb_ack) begin
+          fifo_rd_en <= 1'b1;
           resp_rdata <= wb_dat_r;
-          wb_dat_w <= req_wdata;
           resp_valid <= 1'b1;
-
-          transferred_len <= transferred_len + 'd1;
+          wb_dat_w <= fifo_req_wdata;
 
           // If only one req was accepted or ib_fsm stopped, interrupt operation
-          if ((wb_new_cycle_req && (new_cycle_req_len <= 'd2))
-             || ((((ib_state == IDLE)
-                || (accepted_len_since_last_idle == 'd1))
-                && (transfer_remaining_len <= 'd2)))) begin
+          if (fifo_empty) begin
             // Last accepted/granted beat has just completed.
             wb_cyc   <= 1'b0;
             wb_stb   <= 1'b0;
@@ -207,7 +204,7 @@ always @(posedge clk) begin
             // More already-granted beats remain.
             wb_adr <= wb_adr + 'd4;
 
-            if (transfer_remaining_len <= 'd2) begin
+            if (fifo_last_beat) begin
               wb_cti <= 3'b111;   // next accepted beat is the last one
               wb_state <= FINISH;
             end
@@ -217,12 +214,13 @@ always @(posedge clk) begin
         end
       end
       FINISH: begin
+        fifo_rd_en <= 1'b0;
         if (wb_ack) begin
+          fifo_rd_en <= 1'b1;
           resp_rdata <= wb_dat_r;
           resp_valid <= 1'b1;
-          wb_dat_w <= req_wdata;
+          wb_dat_w <= fifo_req_wdata;
           wb_adr <= 32'h0000_0000;
-          transferred_len <= transferred_len + 'd1;
         end
         wb_cyc <= 1'b0;
         wb_stb <= 1'b0;
