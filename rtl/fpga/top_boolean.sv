6f7d7f9dd1367c0b0a61b9378da47e295171b5bf
Raul Schmidlin <raulschmidlin@gmail.com>
2026-08-21 15:15:54 +0200

    Switching to Boolean board

---
+++ rtl/fpga/top_boolean.sv
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: Apache-2.0
+//
+// Derived in part from ibex-demo-system
+//
+// Copyright lowRISC contributors
+// Copyright 2026 Raul Schmidlin
+
+module top_boolean #(
+    parameter memfile = ""
+) (
+    // These inputs are defined in data/pins_nexysa7.xdc
+    input         IO_CLK,
+    input         IO_RST,
+    input  [15:0] SW,
+    input  [ 3:1] BTN,
+    output [15:0] LED,
+    output [ 5:0] RGB_LED,
+    input         UART_RX,
+    output        UART_TX
+);
+
+  logic clk_sys, rst_sys_n;
+
+  // Instantiating the Ibex Demo System.
+  minsoc_rv_top #(
+      .memfile(memfile)
+  ) minsoc_rv_top_i (
+      .wb_clk_i  (clk_sys),
+      .wb_rst_i  (~rst_sys_n),
+      .tdo_pad_o (),
+      .tms_pad_i (1'b0),
+      .tck_pad_i (1'b0),
+      .tdi_pad_i (1'b0),
+      .uart_srx_i(UART_RX),
+      .uart_stx_o(UART_TX)
+  );
+
+  logic IO_RST_N;
+  assign IO_RST_N = ~IO_RST;
+
+  // Generating the system clock and reset for the FPGA.
+  // Nexys A7 has a 100 MHz clock.
+  clkgen_xil7series clkgen (
+      .IO_CLK,
+      .IO_RST_N,
+      .clk_sys,
+      .rst_sys_n
+  );
+
+endmodule
